b5ba2697e30b6a39c5e1abb07528202c576c1bb0
Sanya Rajput <159819263+sanyarajputt@users.noreply.github.com>
2026-08-11 23:56:58 +0530

    Add: binary-tree-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> result;
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+        result.push_back(root->val);
+        inorder(root->right);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        inorder(root);
+        return result;
+    }
+};
